319f2f9a13971afae6cdd416008dcf0d6062e186
Chase Jensen <soccerchase@gmail.com>
2018-06-18 01:14:35 -0600

    Change combined reducers in src index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import rootReducer from './reducers/balance';
+import rootReducer from './reducers';
 import { Provider } from 'react-redux';
 import App from './components/App';
 
