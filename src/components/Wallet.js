62f5f8452537989845d7c2a8f52796e219548329
Chase Jensen <soccerchase@gmail.com>
2018-06-17 23:28:37 -0600

    Deposit input, unit test, etc

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { deposit } from '../actions/balance';
 
 export class Wallet extends Component {
   constructor() {
@@ -9,15 +10,18 @@ export class Wallet extends Component {
 
   updateBalance = event => this.setState({ balance: parseInt(event.target.value, 10) });
 
+  deposit = () => this.props.deposit(this.state.balance)
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet balance: {this.props.balance}</h3>
         <br />
         <input className="input-wallet" onChange={this.updateBalance}/>
+        <button className='btn-deposit' onClick={this.deposit}>Deposit</button>
       </div>
     )
   }
 }
 
-export default connect(state => { return { balance: state } }, null)(Wallet);
+export default connect(state => { return { balance: state } }, { deposit })(Wallet);
