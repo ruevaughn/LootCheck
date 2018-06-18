34dfaba363ebd32fe69cbcdc33ca04068b8da44e
Chase Jensen <soccerchase@gmail.com>
2018-06-17 23:15:29 -0600

    Ability and test to update wallet amount

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export class Wallet extends Component {
+  constructor() {
+    super();
+    this.state = { balance: undefined };
+  };
+
+  updateBalance = event => this.setState({ balance: parseInt(event.target.value, 10) });
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet balance: {this.props.balance}</h3>
         <br />
-        <input className="input-wallet" />
+        <input className="input-wallet" onChange={this.updateBalance}/>
       </div>
     )
   }
